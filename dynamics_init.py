d187d4a6816f289c77264cc1c1c92f0f8922e899
Alex Baynard <72278803+trippinnotbuggin@users.noreply.github.com>
2024-04-15 15:24:00 -0400

    Update dynamics_init.py

--- dynamics_init.py
+++ dynamics_init.py
@@ -1,53 +1,53 @@
 import numpy as np
 
-def integ(N,bounds, func):
-    """
-    uplim = bounds[1]
-    lowlim = bounds[0]
-    dn = (uplim-lowlim)/N #what we're iterating
-    final = 0
-    for i in range(0,N):
-        final += (dn/2)*(func[0]+2*func[])
-    return final
-    """
-
 
 class dynamicObject:
-    def __init__(self,vel,dist,time,mass):
+    def __init__(self,vel,pos,dist,time,accel,mass, timestep):
         """
         Args:
             self (): self
-            vel (float): average direction array for the density's initial velocity
+            vel (ndarray): average direction array for the objects's initial velocity
             dist (float): distance of the charge density from the center of mass in the system
             time (float): the current time step from simulation start where t=0 in s (or ms?)
             mass (float): object mass in kg
+            timestep(float): constant timestep in the system
         """
         self.vel = vel
+        self.pos = pos
         self.dist = dist
         self.time = time
+        self.accel = accel
         self.mass = mass
+        self.timestep = timestep
 
     def getVelovity(self):
         return self.vel
     
-    def updateVel(self, newVel):
+    def updateAccel(self, force):
+        """
+        updates the acceleration of the object.
+        Args:
+            force(ndarray): total force on the system
+        """
+        newAccel = force/self.mass
+        self.updateVel(newAccel)
+        self.accel = newAccel
+
+    def updateVel(self, newAccel):
+        """
+        updates the velocity based on the new acceleration
+        Args:
+            newAccel(ndArray): acceleration vector
+        """
+        newVel = (self.accel+newAccel)*self.timestep/2
+        self.updatePos(newVel)
         self.vel = newVel
 
-    def getAcceleration(self, prevVels, steps):
-        """
-        Args:
-            prevVels (ndarray): Velocities at the previous timesteps we want to find the acceleration over
-            steps (int): The precision of the integration technique
-        """
-        timestep = 3e-5 #around 30 microseconds
-        endtime = self.time
-        starttime = endtime - (len(prevVels)*timestep)
-        velocities = prevVels.append(self.getVelovity())
-        accel = integ(steps, (starttime, endtime) , velocities)
-        return accel
-
-
-    #def force(self, externalForce):
+    
+    def updatePos(self, newVel):
+        newPos = (self.vel+newVel)*self.timestep/2
+        self.pos = newPos
+    
 
     def getPos(self, dt):
         """
@@ -56,22 +56,21 @@ class dynamicObject:
         """
         self.vel
 
-    #def updatePos(self):
-    
-
 
 class chargeDensity(dynamicObject):
 
-    def __init__(self, charge, vel, dist, time, mass):
+    def __init__(self, charge,Lforce, vel, pos, dist,time, accel, mass, timestep):
         """
         Args:
             self (): self
             charge (float): The total charge of the density
+            Lforce (ndarray): External Lorentz force on the charge density (could be initially 0)
         """
         self.charge = charge
         self.mass = mass
+        self.Lforce = Lforce
         self.__calculateInitVel()
-        dynamicObject.__init__(self, vel, dist, time, mass) 
+        dynamicObject.__init__(self, vel, pos, dist,time, accel, mass, timestep) 
 
     def __calculateInitVel(self):
         """
@@ -90,6 +89,25 @@ class chargeDensity(dynamicObject):
     def getMass(self):
         numb =  #number density of our electrons
 
+    def updateLforce(self, extforce):
+        """
+        args:
+            extforce (ndarry): our new lorentz force vector
+        """
+        updated = self.Lforce - extforce
+        self.updateForce(updated)
+
+    def getcentrifugalForce(self):
+        return self.mass*(self.vel**2/self.dist)
+
+    def updateForce(self, newLforce):
+        updated = newLforce + self.getcentrifugalForce()
+        self.force = updated
+        self.updateAccel(updated)
+
     #def getCurrent(self):
     #    return self.charge*self.vel
+
+    #def getMagField(self):
+    #   return magField
     
